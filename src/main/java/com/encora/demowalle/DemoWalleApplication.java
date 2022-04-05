e24571ded581c6973d83d45eb85de7e049f50656
Oswaldo <bigosvaap@gmail.com>
2022-04-05 17:50:41 -0500

    Testing database connection #1

--- src/main/java/com/encora/demowalle/DemoWalleApplication.java
+++ src/main/java/com/encora/demowalle/DemoWalleApplication.java
@@ -1,10 +1,13 @@
 package com.encora.demowalle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class DemoWalleApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoWalleApplication.class, args);
--- src/test/java/com/encora/demowalle/DummyTests.java
+++ src/test/java/com/encora/demowalle/DummyTests.java
@@ -1,15 +1,13 @@
 package com.encora.demowalle;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
 
 public class DummyTests {
 
     @Test
     void Dummy() {
-        assertNotNull(null);
+        assertEquals(2, 2);
     }
 
 }
