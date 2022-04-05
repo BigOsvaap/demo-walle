3299777b56808911c0bdd0cd940fdfec8c49d14e
Oswaldo <bigosvaap@gmail.com>
2022-04-05 14:38:29 -0500

    Added testing job

---
+++ src/test/java/com/encora/demowalle/DummyTests.java
@@ -0,0 +1,13 @@
+package com.encora.demowalle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class DummyTests {
+
+    @Test
+    void Dummy() {
+        assertEquals(2, 2);
+    }
+
+}
