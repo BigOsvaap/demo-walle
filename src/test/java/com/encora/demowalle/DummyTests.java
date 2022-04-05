6124d3ef2b267c3f3a88105abb8e42f6103079eb
Oswaldo <bigosvaap@gmail.com>
2022-04-05 14:41:28 -0500

    Failing tests

--- src/test/java/com/encora/demowalle/DummyTests.java
+++ src/test/java/com/encora/demowalle/DummyTests.java
@@ -1,13 +1,15 @@
 package com.encora.demowalle;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 
 public class DummyTests {
 
     @Test
     void Dummy() {
-        assertEquals(2, 2);
+        assertNotNull(null);
     }
 
 }
