81fa0dea77c5e8d121fc4b08b7f9037b9b08692c
Oswaldo <bigosvaap@gmail.com>
2022-04-05 22:18:09 -0500

    Testing workflow with PR #1

--- src/main/java/com/encora/demowalle/DemoWalleApplication.java
+++ src/main/java/com/encora/demowalle/DemoWalleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoWalleApplication {
 
+    //Solo soy un comentario para probar el workflow haciendo un PR
 
     public static void main(String[] args) {
         SpringApplication.run(DemoWalleApplication.class, args);
